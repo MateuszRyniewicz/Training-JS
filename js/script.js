5c9a30456def89561d0cb8745fd985d4828bfb28
Mateusz <ryniuss@o2.pl>
2022-05-29 14:45:04 +0200

    task is done

--- js/script.js
+++ js/script.js
@@ -4,3 +4,36 @@ const letters = /[a-z]/i;
 const numbers = /[0-9]/;
 const special = /[!@#$%^&*()]/;
 const minValue = 10;
+
+const showMsg = () => {
+	if (
+		pass.value.length > minValue &&
+		pass.value.match(letters) &&
+		pass.value.match(numbers) &&
+		pass.value.match(special)
+	) {
+		p.textContent = 'Bardzo Dobre hasło';
+		p.style.color = 'lime';
+	} else if (
+		pass.value.length > minValue &&
+		pass.value.match(letters) &&
+		pass.value.match(numbers)
+	) {
+		p.textContent = 'Dobre hasło';
+		p.style.color = 'blue';
+	} else {
+		p.textContent = 'zle hasło';
+		p.style.color = 'red';
+	}
+};
+
+const checkPassword = () => {
+	if (pass.value != '') {
+		showMsg();
+	} else {
+		p.textContent = 'brak hasła';
+		p.style.color = '';
+	}
+};
+
+pass.addEventListener('keyup', checkPassword);
