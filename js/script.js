8b815e29f920eeebb3b237aa0bdf16548146d654
Mateusz <ryniuss@o2.pl>
2022-05-29 10:29:05 +0200

    add new task

--- js/script.js
+++ js/script.js
@@ -1,30 +1,6 @@
-const sizeUp = document.querySelector('.sizeUp');
-const sizeDown = document.querySelector('.sizeDown');
-const color = document.querySelector('.color');
-const pText = document.querySelector('p');
-
-let fontSize = 36;
-
-const SizeUpText = () => {
-	if (fontSize >= 60) return;
-	fontSize += 5;
-	pText.style.fontSize = fontSize + 'px';
-};
-
-const SizeDownText = () => {
-	if (fontSize <= 21) return;
-	fontSize -= 5;
-	pText.style.fontSize = fontSize + 'px';
-};
-
-const ChangeColor = () => {
-	r = Math.floor(Math.random() * 255);
-	g = Math.floor(Math.random() * 255);
-	b = Math.floor(Math.random() * 255);
-
-	pText.style.color = `rgb(${r},${g},${b})`;
-};
-
-sizeUp.addEventListener('click', SizeUpText);
-sizeDown.addEventListener('click', SizeDownText);
-color.addEventListener('click', ChangeColor);
+const pass = document.querySelector('#password');
+const p = document.querySelector('.passinfo');
+const letters = /[a-z]/i;
+const numbers = /[0-9]/;
+const special = /[!@#$%^&*()]/;
+const minValue = 10;
