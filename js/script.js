072d5075ae3f6cf4df6ef89c6e6fa2eabb9a9703
Mateusz <ryniuss@o2.pl>
2022-05-30 19:47:06 +0200

    add all

--- js/script.js
+++ js/script.js
@@ -1,57 +1,13 @@
-const convert = document.querySelector('#converter');
-const result = document.querySelector('.result');
-const convBtn = document.querySelector('.conv');
-const restBtn = document.querySelector('.reset');
-const changeBtn = document.querySelector('.change');
-const one = document.querySelector('.one');
-const two = document.querySelector('.two');
+const burger = document.querySelector('.burger');
+const barsIco = document.querySelector('.fa-bars');
+const xIco = document.querySelector('.fa-times');
+const nav = document.querySelector('nav ul');
 
-let fahren;
-let cel;
-
-const swap = () => {
-	if (one.textContent === '°C') {
-		one.textContent = '°F';
-		two.textContent = '°C';
-		result.textContent = '';
-	} else {
-		one.textContent = '°C';
-		two.textContent = '°F';
-		result.textContent = '';
-	}
+const handleNav = () => {
+	nav.classList.toggle('active');
+	burger.classList.toggle('active');
+	barsIco.classList.toggle('hide');
+	xIco.classList.toggle('hide');
 };
 
-const FarToCel = () => {
-	fahren = convert.value * 1.8 + 32;
-	result.textContent = `${convert.value}C to ${fahren.toFixed(1)}F`;
-	convert.value = '';
-};
-
-const CelToFar = () => {
-	cel = (convert.value - 32) / 1.8;
-	result.textContent = `${convert.value}F to ${cel.toFixed(1)}C`;
-	convert.value = '';
-};
-
-const converter = () => {
-	if (convert.value !== '') {
-		if (one.textContent == '°C') {
-			FarToCel();
-		} else {
-			CelToFar();
-		}
-	} else {
-		result.textContent = 'you have to write number';
-	}
-};
-
-const reseting = () => {
-	convert.value = '';
-	result.textContent = '';
-};
-
-changeBtn.addEventListener('click', swap);
-
-convBtn.addEventListener('click', converter);
-
-restBtn.addEventListener('click', reseting);
+burger.addEventListener('click', handleNav);
