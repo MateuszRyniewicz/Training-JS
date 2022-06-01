ee864376d044cdcee3117cc455ac06d7c2346e5a
Mateusz <ryniuss@o2.pl>
2022-06-01 20:12:42 +0200

    todoList is done

--- js/script.js
+++ js/script.js
@@ -4,6 +4,13 @@ let addBtn; // przycisk ADD= dodaje nowe elementy do listy;
 let ulList; // list zadan, tagi UL;
 let newToDo; //Nowe dodane Li nowe zadanie;
 
+let popup;
+let popupInfo; //infomracje w popapie jak sie poda pusty tekst;
+let toDoToEdit; //edytowanty toDo;
+let toDoInput; //input w popapie;
+let popapAtBtn; //btn w popapie;
+let popapCloseBtn; //bnt zamykajact w popapie;
+
 const main = () => {
 	prepareDOMElements();
 	preapareDOMEvents();
@@ -16,12 +23,22 @@ const prepareDOMElements = () => {
 	errorInfo = document.querySelector('.error-info');
 	addBtn = document.querySelector('.btn-add');
 	ulList = document.querySelector('.todolist ul');
+
+	popup = document.querySelector('.popup');
+	popupInfo = document.querySelector('.popup-info');
+	toDoInput = document.querySelector('.popup-input');
+	popapAtBtn = document.querySelector('.accept');
+	popapCloseBtn = document.querySelector('.cancel');
 };
 
 const preapareDOMEvents = () => {
 	//nadajemy nasluchiwanie
 
 	addBtn.addEventListener('click', addNewTask);
+	ulList.addEventListener('click', checkEvent);
+	popapCloseBtn.addEventListener('click', closePopup);
+	popapAtBtn.addEventListener('click', changeToDoText);
+	todoInput.addEventListener('keyup', enterKeyCheck);
 };
 
 const addNewTask = () => {
@@ -57,6 +74,55 @@ const createTolsArea = () => {
 	btnDelete.innerHTML = '<i class="fas fa-times"></i>';
 
 	toolsPanel.append(btnComplete, btnEdit, btnDelete);
+};
+
+const checkEvent = (e) => {
+	if (e.target.matches('.complete')) {
+		e.target.closest('li').classList.toggle('completed');
+		e.target.classList.toggle('completed');
+	} else if (e.target.matches('.edit')) {
+		editToDo(e);
+	} else if (e.target.matches('.delete')) {
+		deleteToDo(e);
+	}
+};
+
+const editToDo = (e) => {
+	toDoToEdit = e.target.closest('li');
+	toDoInput.value = toDoToEdit.firstChild.textContent;
+	console.log(todoInput.value);
+	popup.style.display = 'flex';
+};
+
+const closePopup = () => {
+	popup.style.display = 'none';
+	popupInfo.textContent = '';
+};
+
+const changeToDoText = () => {
+	if (toDoInput.value !== '') {
+		toDoToEdit.firstChild.textContent = toDoInput.value;
+		popup.style.display = 'none';
+		popupInfo.textContent = '';
+	} else {
+		popupInfo.textContent = 'You have to add some text';
+	}
+};
+
+const deleteToDo = (e) => {
+	e.target.closest('li').remove();
+
+	const allTodos = ulList.querySelectorAll('li');
+	console.log(allTodos);
+
+	if (allTodos.length === 0)
+		errorInfo.textContent = 'You dont have any case to do!';
+};
+
+const enterKeyCheck = (e) => {
+	if (e.key === 'Enter') {
+		addNewTask();
+	}
 };
 
 document.addEventListener('DOMContentLoaded', main);
